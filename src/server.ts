fc064c6891f9f4980ed72cc36e453532aa2858a8
leomoraiscam <leozonnn@gmail.com>
2020-11-04 20:51:53 -0300

    style guide

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+// import routes from './routes';
 
 const app = express();
 
-app.listen(3333, ()=>{
-  console.log(`🚀 Server started on port 3333`)
-})
+app.listen(3333, () => {
+  console.log('🚀 Server started on port 3333');
+});
