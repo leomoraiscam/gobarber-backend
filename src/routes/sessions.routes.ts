6eaf89078ab5bfb12a9fe33eba5f943c946ae163
leomoraiscam <leozonnn@gmail.com>
2020-11-14 19:49:52 -0300

    dealing with errors from the error class

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -4,28 +4,24 @@ import AuthenticatedUserService from '../services/AuthenticateUserService';
 const sessionRouter = Router();
 
 sessionRouter.post('/', async (request, response) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const authenticatedUser = new AuthenticatedUserService();
+  const authenticatedUser = new AuthenticatedUserService();
 
-    const { user, token } = await authenticatedUser.execute({
-      email,
-      password,
-    });
+  const { user, token } = await authenticatedUser.execute({
+    email,
+    password,
+  });
 
-    const serializedUser = {
-      id: user.id,
-      name: user.name,
-      email: user.name,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
+  const serializedUser = {
+    id: user.id,
+    name: user.name,
+    email: user.name,
+    created_at: user.created_at,
+    updated_at: user.updated_at,
+  };
 
-    return response.json({ user: serializedUser, token });
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
+  return response.json({ user: serializedUser, token });
 });
 
 export default sessionRouter;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -10,25 +10,21 @@ const upload = multer(uploadConfig);
 const usersRouter = Router();
 
 usersRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
+  const { name, email, password } = request.body;
 
-    const createUser = new CreateUserService();
+  const createUser = new CreateUserService();
 
-    const user = await createUser.execute({ name, email, password });
+  const user = await createUser.execute({ name, email, password });
 
-    const serializedUser = {
-      id: user.id,
-      name: user.name,
-      email: user.name,
-      created_at: user.created_at,
-      updated_at: user.updated_at,
-    };
+  const serializedUser = {
+    id: user.id,
+    name: user.name,
+    email: user.name,
+    created_at: user.created_at,
+    updated_at: user.updated_at,
+  };
 
-    return response.json(serializedUser);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(serializedUser);
 });
 
 usersRouter.patch(
@@ -36,26 +32,22 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatar = new UpdateUserAvatarService();
-      const user = await updateUserAvatar.execute({
-        user_id: request.user.id,
-        avatarfilename: request.file.filename,
-      });
+    const updateUserAvatar = new UpdateUserAvatarService();
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarfilename: request.file.filename,
+    });
 
-      const serializedUser = {
-        id: user.id,
-        name: user.name,
-        email: user.name,
-        avatar: user.avatar,
-        created_at: user.created_at,
-        updated_at: user.updated_at,
-      };
+    const serializedUser = {
+      id: user.id,
+      name: user.name,
+      email: user.name,
+      avatar: user.avatar,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
 
-      return response.json(serializedUser);
-    } catch (err) {
-      return response.status(400).json({ error: err.message });
-    }
+    return response.json(serializedUser);
   },
 );
 
