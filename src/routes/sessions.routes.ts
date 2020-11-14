401211ed8faa3161d5231bd3e54990d47ac01c92
leomoraiscam <leozonnn@gmail.com>
2020-11-14 13:03:08 -0300

    validating credentials

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+import AuthenticatedUserService from '../services/AuthenticateUserService';
+
+const sessionRouter = Router();
+
+sessionRouter.post('/', async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    const authenticatedUser = new AuthenticatedUserService();
+
+    const { user } = await authenticatedUser.execute({ email, password });
+
+    const serializedUser = {
+      id: user.id,
+      name: user.name,
+      email: user.name,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json({ user: serializedUser });
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
+});
+
+export default sessionRouter;
