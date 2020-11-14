f258121fadd7af6104b7b6eaf238224d582fbcb3
leomoraiscam <leozonnn@gmail.com>
2020-11-14 18:38:41 -0300

    update avatar user

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '../config/upload';
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
 const upload = multer(uploadConfig);
@@ -35,7 +36,26 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    return response.json({ message: 'ok' });
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarfilename: request.file.filename,
+      });
+
+      const serializedUser = {
+        id: user.id,
+        name: user.name,
+        email: user.name,
+        avatar: user.avatar,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+      };
+
+      return response.json(serializedUser);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 
