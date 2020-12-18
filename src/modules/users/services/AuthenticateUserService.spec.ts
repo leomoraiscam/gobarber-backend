df322a3b02749ec32fb5d88e484e2861971f1aac
leomoraiscam <leozonnn@gmail.com>
2020-12-18 15:49:23 -0300

    test in service of authenticated

--- src/modules/users/services/AuthenticateUserService.spec.ts
+++ src/modules/users/services/AuthenticateUserService.spec.ts
@@ -5,7 +5,7 @@ import AuthenticateUserService from './AuthenticateUserService';
 import CreateUserService from './CreateUserService';
 
 describe('AuthenticatedUsers', () => {
-  it('should be able to create a new user', async () => {
+  it('should be able to create authenticate a user', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
     const fakeHashProvider = new FakeHashProvider();
     const authenticateUser = new AuthenticateUserService(
@@ -30,5 +30,50 @@ describe('AuthenticatedUsers', () => {
 
     expect(response).toHaveProperty('token');
     expect(response.user).toEqual(user);
+  });
+
+  it('should not be able to authenticate with non existing user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    expect(
+      authenticateUser.execute({
+        email: 'joh@example.com',
+        password: 'password',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const authenticateUser = new AuthenticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    await createUser.execute({
+      name: 'Jonh Doe',
+      email: 'joh@example.com',
+      password: 'password',
+    });
+
+    expect(
+      authenticateUser.execute({
+        email: 'joh@example.com',
+        password: 'wrong-password',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
