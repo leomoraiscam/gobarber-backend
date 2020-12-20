bc1c4e6652c8268a3675259bbaea2aa6e87e0dc4
leomoraiscam <leozonnn@gmail.com>
2020-12-20 19:49:05 -0300

    resolve bugfix in path

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -9,7 +9,7 @@ import '@shared/container';
 
 const app = express();
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.tmpFolder));
 app.use(routes);
 
 // _ é igual á next
