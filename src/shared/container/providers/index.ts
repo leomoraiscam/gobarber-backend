8cc243b09d48afa02acd773089251ec7dcee84bd
leomoraiscam <leozonnn@gmail.com>
2020-12-29 15:52:09 -0300

    organizing the container and isolating logic

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,15 +1,16 @@
 import { container } from 'tsyringe';
-import IStorageProvider from './StorageProvider/models/IStorageProvader';
-import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
+import './StorageProvider';
+// import IStorageProvider from './StorageProvider/models/IStorageProvader';
+// import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EthrealMailProvider from './MailProvider/implementations/EthrealMailProvider';
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
 
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
+// container.registerSingleton<IStorageProvider>(
+//   'StorageProvider',
+//   DiskStorageProvider,
+// );
 
 container.registerSingleton<IMailTemplateProvider>(
   'MailTemplateProvider',
