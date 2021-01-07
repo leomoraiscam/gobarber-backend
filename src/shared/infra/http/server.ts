b53b1d70be2e360a51b22c557a2cbda6039effa3
leomoraiscam <leozonnn@gmail.com>
2021-01-06 21:13:23 -0300

    enabling cors in the api

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -8,11 +8,13 @@ import AppError from '@shared/errors/AppError';
 import routes from '@shared/infra/http/routes';
 import '@shared/infra/typeorm';
 import '@shared/container';
+import cors from 'cors';
 import rateLimiter from './middlewares/rateLimiter';
 
 const app = express();
 
 app.use(rateLimiter);
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.tmpFolder));
 app.use(routes);
