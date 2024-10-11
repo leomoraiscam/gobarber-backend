0bb3d15492c80b4f69742f71c111bcc1ee65dc88
lmorais <lmorais.contato@gmail.com>
2024-10-10 22:22:14 -0300

    chore: remove comments in storage provider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,17 +2,17 @@ import { container } from 'tsyringe';
 import './StorageProvider';
 import './CacheProvider';
 
-// import IStorageProvider from './StorageProvider/models/IStorageProvader';
-// import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
+import IStorageProvider from './StorageProvider/models/IStorageProvader';
+import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 import IMailProvider from './MailProvider/models/IMailProvider';
 import EthrealMailProvider from './MailProvider/implementations/EthrealMailProvider';
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
 
-// container.registerSingleton<IStorageProvider>(
-//   'StorageProvider',
-//   DiskStorageProvider,
-// );
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  DiskStorageProvider,
+);
 
 container.registerSingleton<IMailTemplateProvider>(
   'MailTemplateProvider',
