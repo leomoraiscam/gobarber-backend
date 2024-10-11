0b0bdc20baade544dc471e881a1a6eccf4033e27
lmorais <lmorais.contato@gmail.com>
2024-10-11 15:15:54 -0300

    refactor: improve faked user repository object

--- src/modules/users/repositories/IUserRepository.ts
+++ src/modules/users/repositories/IUserRepository.ts
@@ -1,11 +1,10 @@
 import User from '@modules/users/infra/typeorm/entities/User';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
-import IFindAllProvidersDTO from '@modules/users/dtos/IFindAllProvidersDTO';
 
 export default interface IUserRepository {
-  findAllProviders(data: IFindAllProvidersDTO): Promise<User[]>;
-  findById(id: string): Promise<User | undefined>;
-  findByEmail(email: string): Promise<User | undefined>;
+  findById(id: string): Promise<User | null>;
+  findByEmail(email: string): Promise<User | null>;
+  findAllProviders(exceptUserId?: string): Promise<User[] | null>;
   create(data: ICreateUserDTO): Promise<User>;
   save(user: ICreateUserDTO): Promise<User>;
 }
