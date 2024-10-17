a98cc87d454baf2614320d998f3158e11a52cbe7
lmorais <lmorais.contato@gmail.com>
2024-10-16 23:20:42 -0300

    refactor: improve user controller to create user controller

---
+++ src/modules/users/infra/http/controllers/CreateUserController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import CreateUserService from '@modules/users/services/CreateUserService';
+import { classToClass } from 'class-transformer';
+
+export default new (class CreateUserController {
+  public async handle(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+    const createUserService = container.resolve(CreateUserService);
+    const user = await createUserService.execute({ name, email, password });
+
+    return response.json(classToClass(user));
+  }
+})();
