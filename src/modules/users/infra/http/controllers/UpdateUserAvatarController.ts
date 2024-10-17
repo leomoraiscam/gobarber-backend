b521c394e5f007484c6ab147136e1c0037c9fb8f
lmorais <lmorais.contato@gmail.com>
2024-10-16 23:22:19 -0300

    refactor: improve update avatar controller to update user avatar controller

---
+++ src/modules/users/infra/http/controllers/UpdateUserAvatarController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+import { classToClass } from 'class-transformer';
+
+export default new (class UpdateUserAvatarController {
+  public async handle(request: Request, response: Response): Promise<Response> {
+    const { id: userId } = request.user;
+    const imageFileName = request.file.filename;
+    const updateUserAvatarService = container.resolve(UpdateUserAvatarService);
+    const user = await updateUserAvatarService.execute({
+      userId,
+      avatar: imageFileName,
+    });
+
+    return response.json(classToClass(user));
+  }
+})();
