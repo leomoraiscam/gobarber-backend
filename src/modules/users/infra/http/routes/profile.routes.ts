8e8cbb4e00d6f4309a88c9da687c9a3a16541f8b
lmorais <lmorais.contato@gmail.com>
2024-10-16 23:23:59 -0300

    refactor: improve user profile route resources

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { celebrate, Segments, Joi } from 'celebrate';
-import ProfileController from '@modules/users/infra/http/controllers/ProfileController';
+import UpdateUserProfileController from '@modules/users/infra/http/controllers/UpdateUserProfileController';
+import ShowProfileUserController from '@modules/users/infra/http/controllers/ShowProfileUserController';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const profileRouter = Router();
 
-profileRouter.get('/', ensureAuthenticated, ProfileController.show);
+profileRouter.get('/', ensureAuthenticated, ShowProfileUserController.handle);
 profileRouter.put(
   '/',
   ensureAuthenticated,
@@ -13,12 +14,12 @@ profileRouter.put(
     [Segments.BODY]: {
       name: Joi.string().required(),
       email: Joi.string().email().required(),
-      old_password: Joi.string(),
+      oldPassword: Joi.string(),
       password: Joi.string(),
-      password_confirmation: Joi.string().valid(Joi.ref('password')),
+      passwordConfirmation: Joi.string().valid(Joi.ref('password')),
     },
   }),
-  ProfileController.update,
+  UpdateUserProfileController.handle,
 );
 
 export default profileRouter;
