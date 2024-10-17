c11ff4cfde29e7819f31c5b5579f443084810759
lmorais <lmorais.contato@gmail.com>
2024-10-16 23:26:30 -0300

    refactor: improve forgot password controller to send forgot password mail controller

---
+++ src/modules/users/infra/http/controllers/SendForgotPasswordMailController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import SendForgotPasswordMailService from '@modules/users/services/SendForgotPasswordMailService';
+
+export default new (class SendForgotPasswordMailController {
+  public async handle(request: Request, response: Response): Promise<Response> {
+    const { email } = request.body;
+    const sendForgotPasswordMailService = container.resolve(
+      SendForgotPasswordMailService,
+    );
+
+    await sendForgotPasswordMailService.execute(email);
+
+    return response.status(200).json();
+  }
+})();
