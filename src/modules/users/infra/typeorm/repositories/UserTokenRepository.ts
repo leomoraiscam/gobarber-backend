ddbe69d1281025949d3c3511dbb1e23a61591db0
lmorais <lmorais.contato@gmail.com>
2024-10-16 21:44:07 -0300

    refactor: improve user tokens implementation

---
+++ src/modules/users/infra/typeorm/repositories/UserTokenRepository.ts
@@ -0,0 +1,29 @@
+import { getRepository, Repository } from 'typeorm';
+import IUserTokenRepository from '@modules/users/repositories/IUserTokenRepository';
+import UserToken from '../entities/UserToken';
+
+class UserTokenRepository implements IUserTokenRepository {
+  private ormRepository: Repository<UserToken>;
+
+  constructor() {
+    this.ormRepository = getRepository(UserToken);
+  }
+
+  public async findByToken(token: string): Promise<UserToken | null> {
+    return this.ormRepository.findOne({
+      where: { token },
+    });
+  }
+
+  public async generate(userId: string): Promise<UserToken> {
+    const userToken = this.ormRepository.create({
+      user_id: userId,
+    });
+
+    await this.ormRepository.save(userToken);
+
+    return userToken;
+  }
+}
+
+export default UserTokenRepository;
