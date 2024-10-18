e4da0452945a229c5fa3ab36f3d87fbdb42d96f1
lmorais <lmorais.contato@gmail.com>
2024-10-18 17:42:19 -0300

    fix: adjust user repositories in tsyringe container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,11 +6,11 @@ import './providers';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
-import IUserRepository from '@modules/users/repositories/IUserRepository';
-import UserRepository from '@modules/users/infra/typeorm/repositories/UserRepository';
+import { IUserRepository } from '@modules/users/repositories/IUserRepository';
+import { UserRepository } from '@modules/users/infra/typeorm/repositories/UserRepository';
 
-import IUserTokenRepository from '@modules/users/repositories/IUserTokenRepository';
-import UserTokenRepository from '@modules/users/infra/typeorm/repositories/UserTokenRepository';
+import { IUserTokenRepository } from '@modules/users/repositories/IUserTokenRepository';
+import { UserTokenRepository } from '@modules/users/infra/typeorm/repositories/UserTokenRepository';
 
 import INotificationsRepository from '@modules/notifications/repositories/INotificationRepository';
 import NotificationsRepository from '@modules/notifications/infra/typeorm/repositories/NotificationsRepository';
