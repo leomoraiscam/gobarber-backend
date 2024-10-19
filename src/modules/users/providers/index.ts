332b09796a47b69f62a09948443961459c666d54
lmorais <lmorais.contato@gmail.com>
2024-10-18 23:09:26 -0300

    refactor: move hash provider to shared folder

--- src/modules/users/providers/index.ts
+++
@@ -1,5 +0,0 @@
-import { container } from 'tsyringe';
-import IHashProvider from './HashProvider/models/IHashProvider';
-import BCryptHashProvider from './HashProvider/implementations/BCrtyptHashProvider';
-
-container.registerSingleton<IHashProvider>('HashProvider', BCryptHashProvider);
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -8,6 +8,8 @@ import IMailProvider from './MailProvider/models/IMailProvider';
 import EthrealMailProvider from './MailProvider/implementations/EthrealMailProvider';
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+import { IHashProvider } from './HashProvider/models/IHashProvider';
+import { BCryptHashProvider } from './HashProvider/implementations/BCryptHashProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
@@ -23,3 +25,5 @@ container.registerInstance<IMailProvider>(
   'MailProvider',
   container.resolve(EthrealMailProvider),
 );
+
+container.registerSingleton<IHashProvider>('HashProvider', BCryptHashProvider);
