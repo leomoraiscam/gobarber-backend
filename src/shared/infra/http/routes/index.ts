18d93a40ed5f815f660155b0cec42bc27a33a11f
lmorais <lmorais.contato@gmail.com>
2024-10-25 22:08:18 -0300

    chore: update users profile resources

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -9,9 +9,9 @@ import { profileRouter } from '@modules/users/infra/http/routes/profile.routes';
 const routes = Router();
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', userRouter);
-routes.use('/session', sessionRouter);
+routes.use('/users/profile', profileRouter);
+routes.use('/sessions', sessionRouter);
 routes.use('/password', passwordRouter);
-routes.use('/profile', profileRouter);
 routes.use('/providers', providersRouter);
 
 export default routes;
