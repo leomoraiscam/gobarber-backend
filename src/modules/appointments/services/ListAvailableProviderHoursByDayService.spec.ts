5f674c3bc7b61e7c86146c16f982ef728d830b13
lmorais <lmorais.contato@gmail.com>
2024-10-29 21:57:21 -0300

    test: add new service to check available hours by day from provider

---
+++ src/modules/appointments/services/ListAvailableProviderHoursByDayService.spec.ts
@@ -0,0 +1,56 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListAvailableProviderHoursByDayService from './ListAvailableProviderHoursByDayService';
+
+describe('ListAvailableProviderHoursByDayService', () => {
+  let fakeAppointmentsRepository: FakeAppointmentsRepository;
+  let listAvailableProviderHoursByDayService: ListAvailableProviderHoursByDayService;
+
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listAvailableProviderHoursByDayService =
+      new ListAvailableProviderHoursByDayService(fakeAppointmentsRepository);
+  });
+
+  it('should be able to list the hours available from provider by day when received correct data', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 20, 11).getTime();
+    });
+
+    await Promise.all([
+      fakeAppointmentsRepository.create({
+        providerId: 'user',
+        userId: '1234',
+        date: new Date(2020, 4, 20, 14, 0, 0),
+      }),
+      fakeAppointmentsRepository.create({
+        providerId: 'user',
+        userId: '1234',
+        date: new Date(2020, 4, 20, 15, 0, 0),
+      }),
+    ]);
+
+    const availableHours = await listAvailableProviderHoursByDayService.execute(
+      {
+        providerId: 'user',
+        year: 2020,
+        month: 5,
+        day: 20,
+      },
+    );
+
+    expect(availableHours).toEqual(
+      expect.arrayContaining([
+        { hour: 8, available: false },
+        { hour: 9, available: false },
+        { hour: 10, available: false },
+        { hour: 11, available: false },
+        { hour: 12, available: true },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
+        { hour: 17, available: true },
+      ]),
+    );
+  });
+});
