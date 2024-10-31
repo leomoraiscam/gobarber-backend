319694eba1a0cbdaef7b8aefc40359b86c9a9c63
lmorais <lmorais.contato@gmail.com>
2024-10-30 22:27:40 -0300

    feat: create data transfer object to list days availability in month by provider

---
+++ src/modules/appointments/dtos/IListAvailableProviderMonthDTO.ts
@@ -0,0 +1,10 @@
+export default interface IListAvailableProviderMonthRequestDTO {
+  providerId: string;
+  month: number;
+  year: number;
+}
+
+export type ListAvailableProviderMonthResponse = Array<{
+  day: number;
+  available: boolean;
+}>;
