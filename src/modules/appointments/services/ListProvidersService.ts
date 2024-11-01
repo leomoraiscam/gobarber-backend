78fadc8c3ca6e312fa58f777bfcdb22a7a889c9a
lmorais <lmorais.contato@gmail.com>
2024-10-31 21:47:43 -0300

    refactor: improve list provider service

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -1,12 +1,8 @@
 import { injectable, inject } from 'tsyringe';
 import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
+import { classToClass } from 'class-transformer';
 import { IUserRepository } from '../../users/repositories/IUserRepository';
 import { User } from '../../users/infra/typeorm/entities/User';
-import { classToClass } from 'class-transformer';
-
-interface IRequest {
-  user_id: string;
-}
 
 @injectable()
 class ListProvidersService {
@@ -17,16 +13,16 @@ class ListProvidersService {
     private cacheProvider: ICacheProvider,
   ) {}
 
-  async execute({ user_id }: IRequest): Promise<User[]> {
+  async execute(userId: string): Promise<User[]> {
     let users = await this.cacheProvider.recover<User[]>(
-      `providers-list:${user_id}`,
+      `providers-list:${userId}`,
     );
 
     if (!users) {
-      users = await this.userRepository.findAllProviders(user_id);
+      users = await this.userRepository.findAllProviders(userId);
 
       await this.cacheProvider.save(
-        `providers-list:${user_id}`,
+        `providers-list:${userId}`,
         classToClass(users),
       );
     }
