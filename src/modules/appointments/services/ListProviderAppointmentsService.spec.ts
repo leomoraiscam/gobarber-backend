484f8810753d92a0891280956fa3c1d97fbfaec1
lmorais <lmorais.contato@gmail.com>
2024-11-02 19:33:28 -0300

    test: add more use cases to appoitments test

--- src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -39,4 +39,57 @@ describe('ListProviderAppointmentsService', () => {
 
     expect(appointments).toEqual([firstAppointment, secondAppointment]);
   });
+
+  it('should be able to cache the list of appointments when occurs an first execution', async () => {
+    const cacheSaveSpy = jest.spyOn(fakeCacheProvider, 'save');
+    const [appointment] = await Promise.all([
+      fakeAppointmentRepository.create({
+        providerId: 'faked-provider',
+        userId: 'faked-user',
+        date: new Date(2020, 4, 20, 14, 0, 0),
+      }),
+    ]);
+
+    await listProviderAppointmentsService.execute({
+      providerId: 'faked-provider',
+      year: 2020,
+      month: 5,
+      day: 20,
+    });
+
+    expect(cacheSaveSpy).toHaveBeenCalledWith(
+      `provider-appointments: faked-provider:2020-5-20`,
+      expect.arrayContaining([appointment]),
+    );
+  });
+
+  it('should be able to retrieve appointments from cache if available when the same has persisted in cache', async () => {
+    const cacheRecoverSpy = jest.spyOn(fakeCacheProvider, 'recover');
+    const [appointment] = await Promise.all([
+      fakeAppointmentRepository.create({
+        providerId: 'faked-provider',
+        userId: 'faked-user',
+        date: new Date(2020, 4, 20, 14, 0, 0),
+      }),
+    ]);
+
+    await fakeCacheProvider.save(
+      `provider-appointments: faked-provider:2020-5-20`,
+      [appointment],
+    );
+
+    const appointments = await listProviderAppointmentsService.execute({
+      providerId: 'faked-provider',
+      year: 2020,
+      month: 5,
+      day: 20,
+    });
+
+    expect(cacheRecoverSpy).toHaveBeenCalledWith(
+      `provider-appointments: faked-provider:2020-5-20`,
+    );
+    expect(appointments).toEqual([
+      { ...appointment, date: appointment.date.toISOString() },
+    ]);
+  });
 });
--- src/modules/appointments/services/ListProvidersService.spec.ts
+++ src/modules/appointments/services/ListProvidersService.spec.ts
@@ -39,4 +39,68 @@ describe('ListProvidersService', () => {
 
     expect(providers).toEqual([firstUser, secondUser]);
   });
+
+  it('should be able to return cached providers if available when occurs an first execution', async () => {
+    const findAllProvidersSpy = jest.spyOn(
+      fakeUserRepository,
+      'findAllProviders',
+    );
+    const [firstUser, secondUser, thirdUser] = await Promise.all([
+      fakeUserRepository.create({
+        name: 'Ronnie Dawson',
+        email: 'teb@wazizin.ph',
+        password: 'password@',
+      }),
+      fakeUserRepository.create({
+        name: 'Myra Potter',
+        email: 'sauva@jadimeb.lc',
+        password: 'password@',
+      }),
+      fakeUserRepository.create({
+        name: 'Brayan Kall',
+        email: 'brayan@example.com',
+        password: 'password@',
+      }),
+    ]);
+
+    await fakeCacheProvider.save(`providers-list:${thirdUser.id}`, [
+      firstUser,
+      secondUser,
+    ]);
+
+    const providers = await listProvidersService.execute(thirdUser.id);
+
+    expect(providers).toEqual([firstUser, secondUser]);
+    expect(findAllProvidersSpy).not.toHaveBeenCalled();
+  });
+
+  it('should save providers in cache when not cached when the same has persisted in cache', async () => {
+    const cacheSaveSpy = jest.spyOn(fakeCacheProvider, 'save');
+    const findAllProvidersSpy = jest.spyOn(
+      fakeUserRepository,
+      'findAllProviders',
+    );
+    const [, , thirdUser] = await Promise.all([
+      fakeUserRepository.create({
+        name: 'Ronnie Dawson',
+        email: 'teb@wazizin.ph',
+        password: 'password@',
+      }),
+      fakeUserRepository.create({
+        name: 'Myra Potter',
+        email: 'sauva@jadimeb.lc',
+        password: 'password@',
+      }),
+      fakeUserRepository.create({
+        name: 'Brayan Kall',
+        email: 'brayan@example.com',
+        password: 'password@',
+      }),
+    ]);
+
+    await listProvidersService.execute(thirdUser.id);
+
+    expect(cacheSaveSpy).toHaveBeenCalledTimes(1);
+    expect(findAllProvidersSpy).toHaveBeenCalledTimes(1);
+  });
 });
